7d6de4f27defef7d2a213c1a819df5f63cd0db3d
Matheos Mattsson <matheos.mattsson@gmail.com>
2020-05-31 13:41:18 +0300

    Refreshing token implemented. Refactored background.js.

--- authPopup.js
+++ authPopup.js
@@ -5,18 +5,32 @@ const init = () => {
     checkAuthenticated();
 }
 
+/**
+ * Opens the authentication link in a new tab
+ */
 const getAuthPin = () => {
     open('https://trakt.tv/oauth/authorize?response_type=code&client_id=a821a5a68529725044d9769db2bd1be1c1ad046447a3ff85b3ac6f6d9f809584&redirect_uri=urn:ietf:wg:oauth:2.0:oob');
 }
 
+/**
+ * Gets the user entered value and authorizes using the code.
+ */
 const authorize = () => {
     let code = document.getElementById('traktID').value;
-    chrome.runtime.sendMessage({
-        action: 'authorize',
-        code: code
-    }, authFeedback);
+
+    if (code.length == 0) {
+        authFeedback('Please enter the PIN!', 'warning');
+    } else {
+        chrome.runtime.sendMessage({
+            action: 'authorize',
+            code: code
+        }, authFeedback);
+    }
 }
 
+/**
+ * Deauthorizes the user by removing the access token from localstorage and sends a revoke POST to the API.
+ */
 const deauthorize = () => {
     const token = localStorage.getItem('access_token');
     chrome.runtime.sendMessage({
@@ -27,7 +41,22 @@ const deauthorize = () => {
     location.reload();
 }
 
-const authFeedback = (msg) => {
+/**
+ * Calls the background for refreshing of token
+ */
+const refreshToken = () => {
+    const refresh_token = localStorage.getItem('refresh_token');
+    chrome.runtime.sendMessage({
+        action: 'refresh',
+        refresh_token: refresh_token
+    }, authFeedback);
+}
+
+/**
+ * Changes the status message shown in the popup
+ * @param {Message to be shown} msg 
+ */
+const authFeedback = (msg, type = 'none') => {
     const div = document.getElementById('feedback');
     if (msg.startsWith('Authenticated')) {
         //Hide menu if already authenticated
@@ -36,22 +65,30 @@ const authFeedback = (msg) => {
         div.setAttribute('class', 'alert alert-success');
         const deAuthBtn = document.getElementById('de_auth');
         deAuthBtn.style.display = 'inline-block';
-    }
-    else if (msg.includes('failed')) {
+    }else if (type == 'warning') {
+        div.setAttribute('class', 'alert alert-warning');
+    } else if (msg.includes('failed')) {
         div.setAttribute('class', 'alert alert-danger');
     }
     div.textContent = msg;
 }
 
+/**
+ * Check if the client already has an access token saved (already authenticated). Display status.
+ * If the token exists but has expired, renew it.
+ */
 const checkAuthenticated = () => {
     if (localStorage.getItem('access_token') != null) {
         const unixTimeNow = Math.floor(Date.now() / 1000);
         const expires = localStorage.getItem('access_token_expires');
-        
-        //TODO: Check if expired, then, refresh token
+
+        //If the expiration date of the token has passed, refresh it
+        if (expires <= unixTimeNow) {
+            refreshToken();
+        }
 
         authFeedback('Authenticated');
-        
+
     } else {
         authFeedback('Not authenticated.');
     }
--- background.js
+++ background.js
@@ -1,24 +1,44 @@
 //Not that secret lol
-const client_id = "a821a5a68529725044d9769db2bd1be1c1ad046447a3ff85b3ac6f6d9f809584";
-const client_secret = "eaea2b7a521a725ed95b87aa6813fb96e66d4b2f755b657566f9932e29e40937";
-const redirect_uri = "urn:ietf:wg:oauth:2.0:oob";
+const client_id = 'a821a5a68529725044d9769db2bd1be1c1ad046447a3ff85b3ac6f6d9f809584';
+const client_secret = 'eaea2b7a521a725ed95b87aa6813fb96e66d4b2f755b657566f9932e29e40937';
+const redirect_uri = 'urn:ietf:wg:oauth:2.0:oob';
+
+const authURL = 'https://api.trakt.tv/oauth/token';
+const revokeURL = 'https://api.trakt.tv/oauth/revoke';
+
+//Default POST request parameters
+const defaultPostParams = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+};
+
+//Default GET request parameters
+const defaultGetParams = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': client_id
+    }
+};
+
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    let params;
     if (request.action == 'authorize') {
-        fetch('https://api.trakt.tv/oauth/token', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    code: request.code,
-                    client_id: client_id,
-                    client_secret: client_secret,
-                    redirect_uri: redirect_uri,
-                    grant_type: 'authorization_code'
-                })
-            })
+        params = JSON.parse(JSON.stringify(defaultPostParams));
+        params.body = JSON.stringify({
+            code: request.code,
+            client_id: client_id,
+            client_secret: client_secret,
+            redirect_uri: redirect_uri,
+            grant_type: 'authorization_code'
+        });
+        fetch(authURL, params)
             .then(result => result.json())
             .then(data => {
                 if (data.access_token) {
@@ -31,63 +51,44 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 }
 
             })
-            .catch(err => sendResponse('Authentication failed. Wrong code?'));
-    }
-    //TODO: Implement this. just copy of authenticate above for now
-    else if (request.action == 'refresh') {
-        fetch('https://api.trakt.tv/oauth/token', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    code: request.code,
-                    client_id: client_id,
-                    client_secret: client_secret,
-                    redirect_uri: redirect_uri,
-                    grant_type: 'authorization_code'
-                })
-            })
+            .catch(() => sendResponse('Authentication failed. Wrong code?'));
+    } else if (request.action == 'refresh') {
+        params = JSON.parse(JSON.stringify(defaultPostParams));
+        params.body = JSON.stringify({
+            refresh_token: request.refresh_token,
+            client_id: client_id,
+            client_secret: client_secret,
+            redirect_uri: redirect_uri,
+            grant_type: 'refresh_token'
+        });
+
+        fetch(authURL, params)
             .then(result => result.json())
             .then(data => {
                 if (data.access_token) {
                     localStorage.setItem('access_token', data.access_token);
                     localStorage.setItem('access_token_expires', data.created_at + data.expires_in);
                     localStorage.setItem('refresh_token', data.refresh_token);
-                    sendResponse('Authenticated with Trakt!');
+                    sendResponse('Re-authenticated with Trakt!');
                 } else {
-                    sendResponse('Authentication failed. Wrong code?');
+                    sendResponse('Refreshing token failed!');
                 }
 
             })
             .catch(err => sendResponse('Authentication failed. Wrong code?'));
-    }
-    else if (request.action == 'revoke') {
-        fetch('https://api.trakt.tv/oauth/revoke', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    token: request.token,
-                    client_id: client_id,
-                    client_secret: client_secret,
-                })
-            })
+    } else if (request.action == 'revoke') {
+        params = JSON.parse(JSON.stringify(defaultPostParams));
+        params.body = JSON.stringify({
+            token: request.token,
+            client_id: client_id,
+            client_secret: client_secret,
+        });
+        fetch(revokeURL, params)
             .then(result => result.json())
-            .catch(err => sendResponse('Deauthorization failed.'));
+            .catch(() => sendResponse('Deauthorization failed.'));
     } else if (request.action == 'get_slug') {
-        fetch(`https://api.trakt.tv/search/imdb/${request.imdbId}`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'trakt-api-version': '2',
-                    'trakt-api-key': client_id
-                }
-            })
+        params = JSON.parse(JSON.stringify(defaultGetParams));
+        fetch(`https://api.trakt.tv/search/imdb/${request.imdbId}`, params)
             .then(result => result.json())
             .then(showData => {
                 if (showData.length >= 1) {
@@ -100,31 +101,20 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             .catch(playError => sendResponse(`FST Error: ${playError}`));
 
     } else if (request.action == 'start' || request.action == 'pause' || request.action == 'stop') {
-        fetch(`https://api.trakt.tv/shows/${request.slug}/seasons/${request.season}/episodes/${request.episode}`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'trakt-api-version': '2',
-                    'trakt-api-key': client_id
-                }
-            })
+        params = JSON.parse(JSON.stringify(defaultGetParams));
+        console.log(params);
+        fetch(`https://api.trakt.tv/shows/${request.slug}/seasons/${request.season}/episodes/${request.episode}`, params)
             .then(result => result.json())
             .then(episodeData => {
                 if (episodeData.title) {
-                    fetch(`https://api.trakt.tv/scrobble/${request.action}`, {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
-                                'trakt-api-version': '2',
-                                'trakt-api-key': client_id
-                            },
-                            body: JSON.stringify({
-                                "episode": episodeData,
-                                "progress": request.progress
-                            })
-                        })
+                    //Readjust params
+                    params.method = 'POST';
+                    params.headers['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
+                    params.body = JSON.stringify({
+                        "episode": episodeData,
+                        "progress": request.progress
+                    });
+                    fetch(`https://api.trakt.tv/scrobble/${request.action}`, params)
                         .then(startResult => startResult.json())
                         .then(startData => {
                             console.log(startData);
